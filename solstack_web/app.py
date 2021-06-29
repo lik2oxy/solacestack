4ed7e4738c7f0b2b4f89960cbc6f44c4f653bbea
arih <ari@example.com>
2021-06-29 15:05:29 +0800

    flow from main to SC prov menu to ansible is working

--- solstack_web/app.py
+++ solstack_web/app.py
@@ -7,23 +7,36 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def main_page():
-    return render_template("main.html")
+    if request.method == "POST":
+        prov_type = request.form["prov_type"]
+        return redirect(f"/{prov_type}")
+    else:
+        return render_template("main.html")
 
 @app.route('/sc', methods=["GET", "POST"])
 def sc_page():
-    return render_template("sc.html")
+    if request.method == "POST":
+        sctoken = request.form["sctoken"]
+        sc_create(sctoken)
+        return "Done"
+    else:
+        return render_template("sc.html")
 
-@app.route('/test')
-def test1():
-    r = ansible_runner.run(private_data_dir='.', playbook='test.yml')
-    # r = ansible_runner.run(private_data_dir='/tmp/demo', host_pattern='localhost', module='shell', module_args='whoami')
+def sc_create(token):
+    r = ansible_runner.run(
+        private_data_dir='.',
+        playbook='ansible-solace/working-with-solace-cloud/playbook.sc-create.yml', 
+        extravars={
+            'WORKING_DIR': '.',
+            'SOLACE_CLOUD_API_TOKEN': token
+            }
+    )
     print("{}: {}".format(r.status, r.rc))
     # successful: 0
     for each_host_event in r.events:
         print(each_host_event['event'])
     print("Final status:")
     print(r.stats)
-    return "check logs"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
